262e2e8350270ff499c62d77009374ec3991a357
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-05-15 12:20:23 +0800

    zip release files after compile

--- build.sh
+++ build.sh
@@ -18,17 +18,25 @@ IDENTIFIER="linux-386 linux-amd64 linux-arm linux-arm64 linux-mips linux-mipsle 
 
 for id in ${IDENTIFIER}; do
 	echo "build ${id}"
-	mkdir ${BASEDIR}/dist/${id}
-	cp -r ${BASEDIR}/init_files/* ${BASEDIR}/dist/${id}/
+	mkdir ${BASEDIR}/dist/NKNMining-${id}
+	cp -r ${BASEDIR}/init_files/* ${BASEDIR}/dist/NKNMining-${id}/
 
-	mkdir -p ${BASEDIR}/dist/${id}/web/
-	cp -r ${BASEDIR}/NKNMining/web/dist/* ${BASEDIR}/dist/${id}/web/
+	mkdir -p ${BASEDIR}/dist/NKNMining-${id}/web/
+	cp -r ${BASEDIR}/NKNMining/web/dist/* ${BASEDIR}/dist/NKNMining-${id}/web/
 
 	id_os=$(echo ${id} | cut -d - -f 1)
 	id_arch=$(echo ${id} | cut -d - -f 2)
 
 	GOPATH=$GOPATH:${BASEDIR}/NKNMining/ GOOS=${id_os} GOARCH=${id_arch} GOMIPS=softfloat go build
-	mv ${BASEDIR}/NKNMining/src/NKNMining/NKNMining ${BASEDIR}/dist/${id}/
+	if [ "${id_os}" = "windows" ]; then
+		mv ${BASEDIR}/NKNMining/src/NKNMining/NKNMining.exe ${BASEDIR}/dist/NKNMining-${id}/
+	else
+		mv ${BASEDIR}/NKNMining/src/NKNMining/NKNMining ${BASEDIR}/dist/NKNMining-${id}/
+	fi
+
+	cd ${BASEDIR}/dist
+	zip -r "NKNMining-${id}.zip" "NKNMining-${id}"
+	cd ${BASEDIR}/NKNMining/src/NKNMining/
 done 
 
 echo 'done! ^_T'
